03adfa8e177b6f16cfbae9785d185bea329fec9c
Iago Puzer <iagopuzer@hotmail.com>
2022-05-10 18:47:42 -0300

    Ultimas atualizações no projeto

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,40 @@
 window.addEventListener('scroll', onScroll)
 
-onScroll()
+
 function onScroll(){   
    showNavOnScroll()
    showBackToTopButtonOnScroll()
+   activateMenu(home)
+   activateMenu(services)
+   activateMenu(about)
+   activateMenu(contact)
 }
+onScroll()
+
+function  activateMenu(section) {
+    const targetLine = scrollY + innerHeight / 2
+  
+    // verificar se a seção passou da linha
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+  
+    // verificar se a base está abaixo da linha alvo
+    const sectionEndsAt = sectionTop + sectionHeight
+    const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+  
+    // limites da seção
+    const sectionBoundaries =
+      sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+  
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+  
+    menuElement.classList.remove('active')
+    if (sectionBoundaries) {
+      menuElement.classList.add('active')
+    }
+  }
 
 function showNavOnScroll(){
     if(scrollY > 0){
